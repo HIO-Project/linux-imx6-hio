14849669263921bc4f3a4e2e4c39c157d0641129
ben <ben_hhj@hotmail.com>
2016-01-21 10:49:17 +0800

    support Yocto 2.0 compile

--- arch/arm/include/asm/ftrace.h
+++ arch/arm/include/asm/ftrace.h
@@ -45,7 +45,8 @@ void *return_address(unsigned int);
 
 #else
 
-extern inline void *return_address(unsigned int level)
+//extern inline void *return_address(unsigned int level)
+static inline void *return_address(unsigned int level)
 {
 	return NULL;
 }
