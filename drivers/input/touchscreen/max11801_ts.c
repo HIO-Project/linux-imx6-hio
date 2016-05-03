34192a7af91704f1db32d20f269358f652d4f6da
ben <ben_hhj@hotmail.com>
2016-05-03 13:55:31 +0800

    add max11801 driver

--- drivers/input/touchscreen/max11801_ts.c
+++ drivers/input/touchscreen/max11801_ts.c
@@ -275,9 +275,9 @@ static void max11801_ts_phy_init(struct max11801_data *data)
 	/* Average X,Y, take 16 samples average eight media sample */
 	max11801_write_reg(client, MESURE_AVER_CONF_REG, 0xff);
 	/* X,Y panel setup time set to 20us */
-	max11801_write_reg(client, PANEL_SETUPTIME_CONF_REG, 0x11);
+	max11801_write_reg(client, PANEL_SETUPTIME_CONF_REG, 0x88);
 	/* Rough pullup time (2uS), Fine pullup time (10us) */
-	max11801_write_reg(client, TOUCH_DETECT_PULLUP_CONF_REG, 0x10);
+	max11801_write_reg(client, TOUCH_DETECT_PULLUP_CONF_REG, 0x75);
 	/* Auto mode init period = 5ms, scan period = 5ms */
 	max11801_write_reg(client, AUTO_MODE_TIME_CONF_REG, 0xaa);
 	/* Aperture X,Y set to +- 4LSB */
